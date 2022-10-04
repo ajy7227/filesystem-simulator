1967f00a240f998cb6dbfc8b29fd82e40c7600aa
ajy7227 <ajy7227@rit.edu>
2022-10-04 14:52:47 -0500

    Converted to classes. Implemented touch, ls, mkdir, and cd

--- filesystem.py
+++ filesystem.py
@@ -1,46 +1,70 @@
-import io
-
 class Directory:
-    def __init__(self, name, isDirectory):
+    def __init__(self, name, parent):
         self.name = name
-        self.isDirectory = isDirectory
+        self.parent = parent
+        self.files = []
+        self.dirs = []
+    
+    #@classmethod
+    def touch(self, name):
+        self.files.append(File(name))
+    
+    #@classmethod
+    def mkdir(self, name):
+        self.dirs.append(Directory(name, self))
+    
+    #ls@classmethod
+    def ls(self):
+        if len(self.files) == 0 and len(self.dirs) == 0:
+            print("Empty! Use touch/mkdir to add contents.")
+        else:
+            if len(self.files) != 0:
+                for f in self.files:
+                    print(f.name)
+            if len(self.dirs) != 0:
+                for d in self.dirs:
+                    print(d.name)
 
 
-files = []
-
-def touch(name):
-    files.append(io.StringIO(name + "\n" + "file" + "\n"))
-
-def ls():
-    for f in files:
-        print(f.getvalue().splitlines()[0])
-
-"""
-Input: directory name
-Format: name, type, other directories
-"""
-def mkdir(name):
-    files.append(io.StringIO(name + "\n" + "dir" + "\n"))
+class File:
+    def __init__(self, name):
+        self.name = name
 
 if __name__ == "__main__":
 
+    cdir = Directory("/", None)
+
     cmd = ""
-    while cmd != "exit":
+    while True:
         cmd = input("Enter command: ")
+        if cmd.lower() == "exit":
+            print("Exiting...")
+            break
         cmd = cmd.split(" ")
         if cmd[0] == "touch":
             if len(cmd) < 2:
                 print("Please specify a file name!")
             else:
-                touch(cmd[1])
+                cdir.touch(cmd[1])
         elif cmd[0] == "ls":
-            ls()
+            cdir.ls()
         elif cmd[0] == "mkdir":
             if len(cmd) < 2:
                 print("Please specify a directory name!")
             else:
-                mkdir(cmd[1])
+                cdir.mkdir(cmd[1])
+        elif cmd[0] == "cd":
+            if len(cmd) < 2:
+                print("Please specify a valid directory name!")
+            else:
+                for d in cdir.dirs:
+                    print(d.name)
+                    if d.name == cmd[1]:
+                        cdir = d
+                        continue
+                    print("Directory not found!")
+
         else:
-            print("Enter one of the following options:\nls\nmkdir\ncd\ntouch")
+            print("Enter one of the following options:\nls\nmkdir\ncd\ntouch\nType 'exit' to quit")
     
-    print("Exiting...")
+    
